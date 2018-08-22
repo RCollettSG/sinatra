bce7bc8916bac514431ef9ec2307a943c835faab
Reece_Collett <rcollett@spartaglobal.com>
2018-08-22 16:47:39 +0100

    Completed REST practice

--- app.rb
+++ app.rb
@@ -1,8 +1,44 @@
 require "sinatra"
 require "sinatra/contrib"
-require "sinatra/reloader" if development?
-# Create first route
 
+# Reloads the server every time the dev environment is saved
+require "sinatra/reloader" if development?
+
+# Create first route
 get "/" do
-  "Hi, World!"
+  "Welcome to my website"
+end
+
+# READ ROUTE
+get "/cars" do
+  "Get all cars"
+end
+
+# CREATE ROUTE
+get "/cars/new" do
+  "Get new form"
+end
+
+post "/cars/new" do
+  "This was added"
+end
+
+# UPDATED ROUTE
+get "/cars/edit" do
+  "Get edit form"
+end
+
+put "/cars/edit" do
+  "This was updated"
+end
+
+# DELETE ROUTE
+delete "/cars/:id" do
+  "This was deleted"
+end
+
+# Will get any ID you put in the path after / unless it meets one of the conditions above
+get "/cars/:id" do
+  id = params[:id]
+  "Display one car #{id}"
 end
