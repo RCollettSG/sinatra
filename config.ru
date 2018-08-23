d79df2d20f973598971d3e7c5d97c4fc7f0a527a
Reece_Collett <rcollett@spartaglobal.com>
2018-08-23 15:21:02 +0100

    Added templates, layout and partials

--- config.ru
+++ config.ru
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/contrib"
 # Reloads the server every time the dev environment is saved
 require "sinatra/reloader" if development?
+require "pry"
 require_relative "./controllers/posts_controller.rb"
 
 # Direct the request to the correct controller
--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -1,19 +1,50 @@
 class PostsController < Sinatra::Base
+  # Sets the root ad the parent directory of the current file
+  set :root, File.join(File.dirname(__FILE__), "..")
 
+  # Sets the views directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+  # Starts the reloader
   configure :development do
     register Sinatra::Reloader
   end
 
+  $posts = [{
+    id: 0,
+    title: "Post 0",
+    post_body: "This is the 0 post"
+  },
+  {
+    id: 1,
+    title: "Post 1",
+    post_body: "This is the second post"
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    post_body: "This is the third post"
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    post_body: "This is the third post"
+  }]
+
   get "/" do
-    "<h1>Homepage</h1>"
+    @title = "Blog posts"
+    @posts = $posts
+    erb :"posts/index"
   end
 
   get "/new" do
-    "New"
+    erb :"posts/new"
   end
 
   get "/:id" do
-    "Get one #{params[:id]}"
+    id = params[:id].to_i
+    @post = $posts[id]
+    erb :"posts/show"
   end
 
   post "/" do
@@ -29,6 +60,6 @@ class PostsController < Sinatra::Base
   end
 
   get "/:id/edit" do
-    "Gets the edit form with data of the selected item"
+    erb :"posts/edit"
   end
 end
